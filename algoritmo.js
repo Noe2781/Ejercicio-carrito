388cb2cada86c3a60cbcfc28cf131d09b9cadef8
noegunner <noesosa81@gmail.com>
2023-05-11 03:55:29 -0300

    primer commit

---
+++ algoritmo.js
@@ -0,0 +1,21 @@
+const cant = document.querySelector("#cantidad");
+const agregarProducto = document.querySelector("#Agregar");
+const totalPesos= document.querySelector("#Total");
+
+const totalProductos= cant * agregarProducto;
+const valor1Producto= 500;
+const totalPe= totalProductos*valor1Producto;
+
+
+
+function agregarCant()
+
+if (cant == 1) {
+    let agregarProducto = totalProductos;
+} else if (cant == 3) {
+    let agregarProducto = totalProductos;
+}else if (cant == 7) {
+    let agregarProducto = totalProductos;
+
+    
+}
